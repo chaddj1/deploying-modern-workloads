97a92e99546021cb8ba4d3d66120fd12f4014163
[chadd justsen] <chaddj.work@gmail.com>
2023-02-15 22:18:08 +0000

    Added .tfvars file

---
+++ lab.tfvars
@@ -0,0 +1,3 @@
+hub_resource_group = "dmw-lab-021523-cjust-hub"
+spoke_resource_group = "dmw-lab-021523-cjust-spoke"
+prefix = "cjust"
--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   tags = {
-    owner   = "YOURNAMEHERE"
+    owner   = "ChaddJ"
     project = "Deploying Modern Workloads"
   }
 
